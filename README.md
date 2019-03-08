e273da37603130c65c61406399d1dfff3a2484af
lyh <1695304848@qq.com>
2019-03-08 13:40:46 +0800

    目录结构搭建和初始路由设计完成

--- README.md
+++ README.md
@@ -1,26 +1,6 @@
-# vue
 
-> ,
-
-## Build Setup
-
-``` bash
-# install dependencies
-npm install
-
-# serve with hot reload at localhost:8080
-npm run dev
-
-# build for production with minification
-npm run build
-
-# build for production and view the bundle analyzer report
-npm run build --report
+## 代码结构
 ```
-
-For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
-
-
 ├── build                      // 构建相关  
 ├── config                     // 配置相关
 ├── node_module                // npm加载所需的项目依赖模块
@@ -29,6 +9,7 @@ For a detailed explanation on how things work, check out the [guide](http://vuej
 │   ├── assets                 // 主题 字体 图片等静态资源
 │   ├── common                 // 全局公用配置
 │   │   ├── config             // 配置全局路由权限和错误捕获
+│   │   ├── mixin              // 一些vue公用的mixin
 │   │   ├── js                 // 编写公有的方法
 │   │   └── style              // 编写公有的样式
 │   ├── components             // 全局公用组件
@@ -38,9 +19,45 @@ For a detailed explanation on how things work, check out the [guide](http://vuej
 │   ├── App.vue                // 入口页面
 │   └── main.js                // 入口 加载组件 初始化等
 ├── static                     // 第三方不打包资源
-│   └── HT                     // HT组件的配置
 ├── .babelrc                   // babel-loader 配置
 ├── eslintrc.js                // eslint 配置项
 ├── .gitignore                 // git 忽略项
 ├── index.html                 // html模板
 └── package.json               // package.json
+```
+
+
+## 功能相关介绍
+|模块|功能|页面编码|描述|
+|:---|:--|:------|:---|
+|登录|登录|login|菜单中不显示|
+|401|401|401|角色无访问权限时进入这个页面|
+|404|404|404|访问菜单不存在时进入这个页面|
+|首页|首页|home||
+|运维中心||B04||
+|-|问答管理|B0401||
+|-|专栏管理|B0402||
+|-|文章管理|B0403||
+|-|讲堂管理|B0404||
+|-|活动管理|B0405||
+|文件库||B03||
+|-|图片管理|B0301||
+|-|文件管理|B0302||
+|论坛配置||B02||
+|-|首页轮播|B0201||
+|-|技术频道|B0202||
+|-|提示语|B0203||
+|-|广告位|B0204||
+|-|讲座推荐|B0205||
+|-|活动推荐|B0206||
+|-|标签类型管理|B0207||
+|-|标签管理|B0208||
+|系统管理||B01||
+|-|用户管理|B0101||
+|-|角色管理|B0102||
+|-|菜单管理|B0103||
+|-|区域管理|B0104||
+|-|字典管理|B0105||
+|-|系统日志|B0106||
+
+## 基本业务分析
--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 // 布局组件
-// import horizontal from '@/views/layout/horizontal'
-// import vertical from '@/views/layout/vertical'
+import Layout from '@/views/layout/index'
 
 Vue.use(Router)
 
@@ -13,8 +12,7 @@ Vue.use(Router)
  * component 映射页面
  * name 路由名字 (唯一，不可重复，相当于唯一标识)
  * hidden 设置为true则加载菜单的时候不渲染这个路由
- * type 用来区分是平台级的路由还是项目级的路由
- * meta: { // 这个是菜单渲染的时候用到的参数
+ * meta: { // 自定义参数，这里用来做菜单渲染
  *    title 菜单渲染显示的名字
  *    icon  菜单图标
  * }
@@ -32,12 +30,177 @@ export const constantRouterMap = [
     hidden: true,
     meta: {title: '登录', icon: ''}
   },
-  // 默认进入的页面
+  // 首页
   {
-    path: '/',
-    component: () => import('@/views/home/index'),
-    hidden: true,
-    type: 'platform'
+    path: '',
+    component: Layout,
+    redirect: '/home',
+    children: [
+      {
+        path: '/home',
+        component: () => import('@/views/home/index'),
+        name: 'home',
+        meta: {title: '首页', icon: ''}
+      }
+    ]
+  },
+  // 运维中心
+  {
+    path: '/B04',
+    component: Layout,
+    redirect: '/B04/B0401',
+    children: [
+      {
+        path: '/B0401',
+        component: () => import('@/views/B04/B0401/index'),
+        name: 'B0401',
+        meta: {title: '问答管理', icon: ''}
+      },
+      {
+        path: '/B0402',
+        component: () => import('@/views/B04/B0402/index'),
+        name: 'B0402',
+        meta: {title: '专栏管理', icon: ''}
+      },
+      {
+        path: '/B0403',
+        component: () => import('@/views/B04/B0403/index'),
+        name: 'B0403',
+        meta: {title: '文章管理', icon: ''}
+      },
+      {
+        path: '/B0404',
+        component: () => import('@/views/B04/B0404/index'),
+        name: 'B0404',
+        meta: {title: '讲堂管理', icon: ''}
+      },
+      {
+        path: '/B0405',
+        component: () => import('@/views/B04/B0405/index'),
+        name: 'B0405',
+        meta: {title: '活动管理', icon: ''}
+      }
+    ]
+  },
+  // 文件库
+  {
+    path: '/B03',
+    component: Layout,
+    redirect: '/B03/B0301',
+    children: [
+      {
+        path: '/B0301',
+        component: () => import('@/views/B03/B0301/index'),
+        name: 'B0301',
+        meta: {title: '图片管理', icon: ''}
+      },
+      {
+        path: '/B0302',
+        component: () => import('@/views/B03/B0302/index'),
+        name: 'B0302',
+        meta: {title: '文件管理', icon: ''}
+      }
+    ]
+  },
+  // 论坛配置
+  {
+    path: '/B02',
+    component: Layout,
+    redirect: '/B02/B0201',
+    children: [
+      {
+        path: '/B0201',
+        component: () => import('@/views/B02/B0201/index'),
+        name: 'B0201',
+        meta: {title: '首页轮播', icon: ''}
+      },
+      {
+        path: '/B0202',
+        component: () => import('@/views/B02/B0202/index'),
+        name: 'B0202',
+        meta: {title: '技术频道', icon: ''}
+      },
+      {
+        path: '/B0203',
+        component: () => import('@/views/B02/B0203/index'),
+        name: 'B0203',
+        meta: {title: '提示语', icon: ''}
+      },
+      {
+        path: '/B0204',
+        component: () => import('@/views/B02/B0204/index'),
+        name: 'B0204',
+        meta: {title: '广告位', icon: ''}
+      },
+      {
+        path: '/B0205',
+        component: () => import('@/views/B02/B0205/index'),
+        name: 'B0205',
+        meta: {title: '讲座推荐', icon: ''}
+      },
+      {
+        path: '/B0206',
+        component: () => import('@/views/B02/B0206/index'),
+        name: 'B0206',
+        meta: {title: '活动推荐', icon: ''}
+      },
+      {
+        path: '/B0207',
+        component: () => import('@/views/B02/B0207/index'),
+        name: 'B0207',
+        meta: {title: '标签类型管理', icon: ''}
+      },
+      {
+        path: '/B0208',
+        component: () => import('@/views/B02/B0208/index'),
+        name: 'B0208',
+        meta: {title: '标签管理', icon: ''}
+      }
+    ]
+  },
+  // 系统管理
+  {
+    path: '/B01',
+    component: Layout,
+    redirect: '/B01/B0101',
+    children: [
+      {
+        path: '/B0101',
+        component: () => import('@/views/B01/B0101/index'),
+        name: 'B0101',
+        meta: {title: '用户管理', icon: ''}
+      },
+      {
+        path: '/B0102',
+        component: () => import('@/views/B01/B0102/index'),
+        name: 'B0102',
+        meta: {title: '角色管理', icon: ''}
+      },
+      {
+        path: '/B0103',
+        component: () => import('@/views/B01/B0103/index'),
+        name: 'B0103',
+        meta: {title: '菜单管理', icon: ''}
+      },
+      {
+        path: '/B0104',
+        component: () => import('@/views/B01/B0104/index'),
+        name: 'B0104',
+        meta: {title: '区域管理', icon: ''}
+      },
+      {
+        path: '/B0105',
+        component: () => import('@/views/B01/B0105/index'),
+        name: 'B0105',
+        meta: {title: '字典管理', icon: ''}
+      },
+      {
+        path: '/B0106',
+        component: () => import('@/views/B01/B0106/index'),
+        name: 'B0106',
+        meta: {title: '系统日志', icon: ''}
+      }
+    ]
   },
   // 错误页面定义
   {
